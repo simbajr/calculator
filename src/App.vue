e70acef312c09f73a75f1354f32ac904cd4de1a5
Charles Simba <charles.simba@knowit.se>
2022-05-11 10:22:49 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,105 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div class="hello">
+
+    <div>
+      <h1 class="title"> The Calculator </h1>
+    </div>
+
+    <hr>
+    <div class="container">
+      <div>
+        <div class="col">
+          <form>
+
+            <div class="col-sm-12">
+              <label class="col-sm-2"> First number </label>
+              <input type="text" class="form-group col-sm-2" v-model="num1" placeholder="Num 1">
+            </div>
+
+            <div style="margin-top:13px" class="col-sm-12">
+              <label class="col-sm-2"> Second number </label>
+              <input type="text" class="form-group col-sm-2" v-model="num2" placeholder="Num 2">
+
+            </div>
+
+          </form>
+        </div>
+        <div class="col">
+          <div class="col-sm-12">
+            <div><label class="col-sm-2">Addition: {{ add }}</label></div>
+            <div><label class="col-sm-2">Multiplication: {{ mul }}</label></div>
+            <div><label class="col-sm-2">Subtraction: {{ sub }}</label></div>
+            <div><label class="col-sm-2">Division: {{ div }}</label></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="col-sm-4">
+          <button type="button" class="btn btn-primary" v-on:click="postRequest()">Calcuclate</button>
+        </div>
+      </div>
+    </div>
+    &nbsp;
+
+    &nbsp;
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import axios from 'axios'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: function () {
+    return {
+      add: "", mul: "", sub: "", div: "",
+      num1: "", num2: ""
+    }
+  },
+  methods: {
+    postRequest() {
+      var data = { "num1": parseFloat(this.num1), "num2": parseFloat(this.num2) }
+
+
+      console.log(data)
+
+      axios({ method: "POST", url: "http://127.0.0.1:8088/calc", data: data, headers: { "content-type": "text/plain" } }).then(result => {
+        //caseSensative
+        this.add = result.data['Add']
+        this.mul = result.data['Mul']
+        this.sub = result.data['Sub']
+        this.div = result.data['Div']
+
+        console.log(result.data)
+      }).catch(error => {
+        console.log(error)
+      })
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// },
+// devServer: {
+//   proxy:'https://localhost:8088'
+
+// })
+
+module.exports = {
+  devServer: {
+    proxy: {
+      "^/api": {
+        target: "http://127.0.0.1:8088",
+        changeOrigin: true,
+        logLevel: "debug",
+        pathRewrite: { "^/api": "/" }
+      }
+    }
+  }
+};
